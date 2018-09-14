276d6c8faf0ace069dc5abfdc447c4439b853fdf
davieu <davisumana@gmail.com>
2018-09-13 22:02:12 -0400

    controllers working and logging values

--- app.js
+++ app.js
@@ -14,14 +14,48 @@ var budgetController = (function() {
 
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 var controller = (function(budgetCtrl, UIctrl){
-    var z = budgetCtrl.publicTest(5);
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var DOM = UIctrl.getDOMstrings();
+
+    var ctrlAddItem = function() {
+        //get the field input data
+        //add the item to the budget controller
+        //add the item to the ui
+        //calculate the budget
+        //display the budget on the UI
+        var input = UIctrl.getInput();
+        console.log(input);
     }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
+
 })(budgetController, UIController);
